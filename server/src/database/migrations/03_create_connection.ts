332f7c40da68f0bcc4c9e6b142680e3357f42d56
Maycon <m.vinnicyus@gmail.com>
2020-08-04 15:54:45 -0300

    feat: setting up database

---
+++ server/src/database/migrations/03_create_connection.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+
+export const up = async (knex: Knex) => {
+  return knex.schema.createTable('connections', table => {
+    table.increments('id').primary();
+
+    table
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    table.timestamp('created_at').defaultTo('now()').notNullable();
+  });
+};
+
+export const down = async (knex: Knex) => {
+  return knex.schema.dropTable('connections');
+};
