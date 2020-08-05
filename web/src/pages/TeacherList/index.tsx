08e1e94c3dee49a56f47f4297f66af509f647f81
Maycon <m.vinnicyus@gmail.com>
2020-08-05 15:56:26 -0300

    feat: Select subject and week day

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,14 +4,42 @@ import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <Input label="Matéria" name="subject" />
-          <Input label="Dia da semana" name="week_day" />
+          <Select
+            label="Matéria"
+            name="subject"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Sociologia', label: 'Sociologia' },
+              { value: 'Filosofia', label: 'Filosofia' },
+              { value: 'Física', label: 'Física' },
+            ]}
+          />
+          <Select
+            label="Dia da semana"
+            name="week_day"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
           <Input label="Hora" name="time" type="time" />
         </form>
       </PageHeader>
