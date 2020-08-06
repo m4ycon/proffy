9081712e504a567c57c142549a50214b74b6f762
Maycon <m.vinnicyus@gmail.com>
2020-08-06 17:56:58 -0300

    style: focused color tabButton

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -40,8 +40,12 @@ function StudyTabs() {
       <Screen
         options={{
           tabBarLabel: 'Proffys',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="ios-easel" size={size} color={color} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="ios-easel"
+              size={size}
+              color={focused ? '#8257e5' : color}
+            />
           ),
         }}
         name="TeacherList"
@@ -51,8 +55,12 @@ function StudyTabs() {
       <Screen
         options={{
           tabBarLabel: 'Favoritos',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="ios-heart" size={size} color={color} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="ios-heart"
+              size={size}
+              color={focused ? '#8257e5' : color}
+            />
           ),
         }}
         name="Favorites"
